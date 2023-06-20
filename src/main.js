563d5181b33969ec790f37e512fcf9645438ebf2
Nurullah-Nergiz <developer@nurullah.aleeas.com>
2023-06-20 23:49:49 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,39 +1,44 @@
+import yt from "yt-converter";
 import "./utils/params.js";
 
-// const YoutubeMp3Downloader = require("youtube-mp3-downloader");
+const { url, format, help } = process.params;
 
-/* 
-if (!videoURL || !outputFileName) {
-   console.log("Video URL and output file name are required.");
-   console.log("Usage: node youtube-mp3-downloader.js <video URL> <output file name>");
-   process.exit(1);
+if (!help) {
+   yt.getInfo(url).then((info) => {
+      console.log("Downloading" + info.title + "." + format);
+      if (format === "mp3") {
+         yt.convertAudio(
+            {
+               url,
+               directoryDownload: process.cwd(),
+               itag: 140,
+               title: info.title,
+            },
+            (data) => {
+               console.log("1 ", data);
+            },
+            (data) => {
+               console.log("2 ", data);
+            }
+         );
+      } else if (format === "mp4") {
+         yt.convertVideo(
+            {
+               url,
+               directoryDownload: process.cwd(),
+               itag: 136,
+               title: info.title,
+            },
+            (data) => {
+               console.clear();
+               console.log(info.title + "." + format);
+               console.log("%" + data.percentage);
+            },
+            (data) => console.log(info.title)
+         );
+      }
+   });
+} else {
+   console.log("--url : youtube video link");
+   console.log("--format : video format (mp3,mp4)");
 }
-
-const downloadMp3 = (videoURL, outputFileName) => {
-   const YD = new YoutubeMp3Downloader({
-      ffmpegPath: "/usr/local/bin/ffmpeg", // ffmpeg'in yolunu kendi sisteminizde uygun şekilde değiştirin
-      outputPath: "./", // Çıktı dosyasının kaydedileceği dizin
-      youtubeVideoQuality: "highest", // YouTube video kalitesi
-      queueParallelism: 2, // Aynı anda indirilecek maksimum dosya sayısı
-      progressTimeout: 2000, // İndirme ilerlemesi güncelleme sıklığı (ms)
-   });
-
-   YD.download(videoURL);
-
-   YD.on("finished", (err, data) => {
-      console.log(`MP3 downloaded successfully: ${data.file}`);
-   });
-
-   YD.on("error", (error) => {
-      console.log("An error occurred while downloading the MP3:", error);
-   });
-
-   YD.on("progress", (progress) => {
-      console.log(`Downloading: ${progress.videoId} (${progress.progress.percentage.toFixed(2)}%)`);
-   });
-};
-
-downloadMp3(videoURL, outputFileName);
-
-// MP3'i indirirken bir hata oluştu: Spawn/usr/local/bin/ffmpeg enoent
-*/
--- src/utils/params.js
+++ src/utils/params.js
@@ -1,9 +1,9 @@
 process.params = {};
-process.params = {};
-process.argv.splice(2, 2).map((argv) => {
-   if (argv[0] === "-") {
-      const [key, val] = argv.replace("-", "").split("=");
-      process.params[key] = val;
+process.argv.splice(2, 2).forEach((argv) => {
+   // console.log(argv);
+   if (argv.startsWith("-") || argv.startsWith("--")) {
+      const [key, ...val] = argv.replace("-", "").split("=");
+      process.params[key] = val.join("=");
    }
 });
 
